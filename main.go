045b4fe6b4a809ccb32e4423f6e817adca52686b
Jakob Stadlhuber <jakob@stadlhuber.org>
2021-03-25 15:01:31 +0100

    added basic structure

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
-import "fmt"
+
+import "os"
 
 func main() {
-	fmt.Println("hello world")
+	a := App{}
+	a.Initialize(
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PASSWORD"),
+		os.Getenv("APP_DB_NAME"))
+
+	a.Run(":8010")
 }
